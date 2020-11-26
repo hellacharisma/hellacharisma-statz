52fa435d689327f3920ffac3317331897036ee22
hellacharisma <73649464+hellacharisma@users.noreply.github.com>
2020-11-26 08:04:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,23 @@
 </head>
 
 <body>
-Hello world
+<h1>Hello world</h1>
+
+  <?php
+
+  $host = "us-cdbr-east-02.cleardb.com/heroku_8af758a5bfbbeef";
+  $username = "b2d27009ffc4be";
+  $password = "d1c00c7a";
+  $database = "heroku_8af758a5bfbbeef";
+
+  $mysqli = new mysqli($host, $username, $password, $database);
+  if ($mysqli->connect_errno) {
+      echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  }
+  echo $mysqli->host_info . "\n";
+  
+  ?>
+  
 </body>
 
 </html>
